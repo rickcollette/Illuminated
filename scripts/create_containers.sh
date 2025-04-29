32c181e3a8be100b612054fb0853902d431ed8e3
Rick Collette <rcollet@gmail.com>
2025-04-28 20:49:41 -0500

    hopefully fixed - keeping -x for debugging

--- scripts/create_containers.sh
+++ scripts/create_containers.sh
@@ -17,9 +17,9 @@ STORAGE="local"
 BRIDGE="vmbr0"
 VMID=200
 
-echo "VERSION: 1.0.2"
+echo "VERSION: 1.0.3"
 
-# Function to find latest Ubuntu 24.04 template
+# Find latest Ubuntu 24.04 template
 function find_latest_template() {
   pveam update
   TEMPLATE_NAME=$(pveam available | grep ubuntu-24.04-standard | sort | tail -n 1 | awk '{print $2}')
@@ -39,22 +39,21 @@ function ensure_template() {
 
 # Main logic
 TEMPLATE_NAME=$(find_latest_template)
-TEMPLATE="$STORAGE:vztmpl/$TEMPLATE_NAME"
 ensure_template "$TEMPLATE_NAME"
 
 # Now create containers
 for name in "${!containers[@]}"; do
   echo "📦 Creating LXC: $name (VMID $VMID)..."
-pct create $VMID $TEMPLATE \
-  -hostname $name \
-  -storage $STORAGE \
-  -rootfs ${STORAGE}:8G \
-  -memory ${containers[$name]} \
-  -cores 2 \
-  -net0 name=eth0,bridge=$BRIDGE,ip=dhcp \
-  -unprivileged 1 \
-  -features nesting=1
-pct start $VMID
+  pct create $VMID -ostemplate $TEMPLATE_NAME \
+    -hostname $name \
+    -storage $STORAGE \
+    -rootfs ${STORAGE}:8G \
+    -memory ${containers[$name]} \
+    -cores 2 \
+    -net0 name=eth0,bridge=$BRIDGE,ip=dhcp \
+    -unprivileged 1 \
+    -features nesting=1
+  pct start $VMID
   ((VMID++))
 done
 
