72e7ce4d328fb4eee8c87d5a87f2da5c4b6fd853
Rick Collette <rcollet@gmail.com>
2025-04-28 21:04:28 -0500

    updated template storgae

--- scripts/create_containers.sh
+++ scripts/create_containers.sh
@@ -4,15 +4,16 @@ source <(curl -fsSL https://raw.githubusercontent.com/rickcollette/illuminated/m
 header_info "Creating LXC Containers"
 catch_errors
 
-# Storage and Bridge settings
+# Settings
+TEMPLATE_STORAGE="local"
 STORAGE="local-lvm"
 BRIDGE="vmbr0"
 TAGS="minecraft"
 
-# Latest Ubuntu 24.04 Template
+# Latest Template
 TEMPLATE_NAME=$(pveam available | grep ubuntu-24.04-standard | sort | tail -n 1 | awk '{print $2}')
-pveam download $STORAGE $TEMPLATE_NAME || true
-OSTEMPLATE="$STORAGE:vztmpl/$TEMPLATE_NAME"
+pveam download $TEMPLATE_STORAGE $TEMPLATE_NAME || true
+OSTEMPLATE="${TEMPLATE_STORAGE}:vztmpl/${TEMPLATE_NAME}"
 
 CTID=200
 
@@ -31,7 +32,15 @@ for container in "${!containers[@]}"; do
   DISK=8
 
   msg_info "Creating $HOSTNAME (CTID $CTID)..."
-  build_container
+  pct create ${CTID} ${OSTEMPLATE} \
+    --rootfs ${STORAGE}:${DISK} \
+    --hostname ${HOSTNAME} \
+    --memory ${MEMORY} \
+    --cores ${CORES} \
+    --net0 name=eth0,bridge=${BRIDGE},ip=dhcp \
+    --unprivileged 1 \
+    --features nesting=1 \
+    --tags ${TAGS}
   pct start $CTID
   msg_ok "$HOSTNAME started!"
   ((CTID++))
